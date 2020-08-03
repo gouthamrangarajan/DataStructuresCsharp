d196233a087f0f550e29703490081a245361cad5
Goutham Rangarajan <grangarajan@ana-data.com>
2020-08-03 17:21:38 -0400

    Graph DFS all airport connection problem

---
+++ AirRoutesGraph/AirRoutesGraph/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Find all routes from Point A to point B
+/// </summary>
+namespace AirRoutesGraph
+{
+    class AirMap
+    {
+        private Dictionary<string, Node> _nodeLookUp = new Dictionary<string, Node>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            AirMap graph = new AirMap();
+            graph.AddRoute("A", "B");
+            graph.AddRoute("B", "A");
+            graph.AddRoute("C", "D");
+            graph.AddRoute("A", "C");
+            graph.AddRoute("B", "C");
+            graph.AddRoute("C", "B");
+            graph.AddRoute("D", "B");
+            graph.AddRoute("C", "A");
+            graph.AddRoute("D", "A");
+            var check= graph.GetAllRoutes("A", "D");
+            check.ForEach(dt =>
+            {
+                Console.WriteLine(dt);
+            });
+            if (check.Count == 0)
+            {
+                Console.WriteLine("No routes available");
+            }
+            Console.ReadLine();
+        }
+
+        private List<string> GetAllRoutes(string source, string destination)
+        {
+            List<string> allRoutes = new List<string>();
+            var srcNode = GetNode(source);
+            var destNode = GetNode(destination);            
+            GetAllRouteRecursive(srcNode, destNode, allRoutes,new HashSet<string>(),source);
+            return allRoutes;
+        }
+
+
+        private void GetAllRouteRecursive(Node srcNode, Node destNode, List<string> allRoutes, HashSet<string> visited, string prefix)
+        {
+            if (srcNode == null || destNode == null)
+                return;
+
+            if (visited.Contains(srcNode.Route))
+                return;
+
+            visited.Add(srcNode.Route);
+
+            if (srcNode == destNode)
+            {
+                allRoutes.Add(prefix);
+            }
+            else
+            {
+                foreach (var adj in srcNode.Adjacent)                
+                    GetAllRouteRecursive(adj, destNode, allRoutes, visited, prefix + "," + adj.Route);                                   
+            }
+            visited.Remove(srcNode.Route);
+        }
+
+        void AddRoute(string source,string destination)
+        {
+            var srcnode=GetNode(source);
+            if (srcnode == null)
+            {
+                srcnode = new Node(source);
+                _nodeLookUp.Add(source, srcnode);
+            }
+            var destNode = GetNode(destination);
+            if (destNode == null)
+            {
+                destNode = new Node(destination);
+                _nodeLookUp.Add(destination, destNode);
+            }
+            srcnode.AddAdjacent(destNode);
+
+        }
+
+        private Node GetNode(string route)
+        {
+            if (_nodeLookUp.ContainsKey(route))
+                return _nodeLookUp[route];
+            else
+                return null;
+        }
+    }
+
+    public class Node
+    {
+        public string Route;
+        public List<Node> Adjacent = new List<Node>();
+        public Node(string route)
+        {
+            Route = route;
+        }
+
+        internal void AddAdjacent(Node node)
+        {
+            if (!Adjacent.Contains(node))
+                Adjacent.Add(node);
+        }
+    }
+    
+}

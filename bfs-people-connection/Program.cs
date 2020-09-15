6303eac7c55e06e51b5135677315e1f4198628e4
Goutham Rangarajan <grangarajan@ana-data.com>
2020-09-15 18:33:05 -0400

    code reorg

--- bfs-people-connection/Program.cs
+++ bfs-people-connection/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace bfs_people_connection
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
@@ -14,7 +14,7 @@ namespace bfs_people_connection
             cp.AddConnection("Sam","Mark");
             cp.AddConnection("Adam","Simon");
             cp.AddConnection("Timothy","Simon");
-            var test=cp.GetShortestConnection("Mark","Timothy");
+            var test=cp.GetShortestConnection("simon","sam");
             if(string.IsNullOrWhiteSpace(test) || !test.Contains("=>"))
                 Console.WriteLine("No connection exits");
             else
@@ -22,84 +22,87 @@ namespace bfs_people_connection
             Console.ReadLine();
         }
     }
-
     public class ConnectPeople{
-        private Dictionary<string,Node> graph=new Dictionary<string, Node>();
-
+        private Dictionary<string,Node> _nodeLookup=new Dictionary<string, Node>();
         private Node GetNode(string name){
-            if(graph.ContainsKey(name.ToLower()))
-                return graph[name.ToLower()];
-            else
+            if(string.IsNullOrWhiteSpace(name))
                 return null;
+            var nameLowerCase=name.Trim().ToLower();
+            if(_nodeLookup.ContainsKey(nameLowerCase)){
+                return _nodeLookup[nameLowerCase];
+            }
+            return null;
         }
         private Node AddNode(string name){
-            graph.Add(name.ToLower(),new Node(name));
-            return graph[name.ToLower()];
+            if(string.IsNullOrWhiteSpace(name))
+                return null;
+            var nameLowerCase=name.Trim().ToLower();
+            if(!_nodeLookup.ContainsKey(nameLowerCase)){
+                _nodeLookup.Add(nameLowerCase,new Node(name));
+            }
+            return _nodeLookup[nameLowerCase];
         }
         public void AddConnection(string name1,string name2){
-            if(string.IsNullOrWhiteSpace(name1)|| string.IsNullOrWhiteSpace(name2))
-                return;            
-            Node nd1=GetNode(name1);
-            if(nd1==null)
+            if(string.IsNullOrWhiteSpace(name1) || string.IsNullOrWhiteSpace(name2))
+                return;
+             var nd1=GetNode(name1);
+             if(nd1==null)
                 nd1=AddNode(name1);
-            Node nd2=GetNode(name2);
+            
+            var nd2=GetNode(name2);
             if(nd2==null)
                 nd2=AddNode(name2);
-            nd1.AddConnection(nd2);
-            nd2.AddConnection(nd1);
-        }
-        public string GetShortestConnection(string name1,string name2){
-            if(string.IsNullOrWhiteSpace(name1)|| string.IsNullOrWhiteSpace(name2))
-                return null;            
-            var nd1=GetNode(name1);
-            var nd2=GetNode(name2);
-            if(nd1==null || nd2==null)
-                return null;
-            var connection=nd1.Name;
-            Queue<KeyValuePair<string,Node>> queue=new Queue<KeyValuePair<string,Node>>();
-            HashSet<Node> visited=new HashSet<Node>();
-            queue.Enqueue(new KeyValuePair<string, Node>(connection,nd1));
-            while(queue.Count>0){
-                var kvp=queue.Dequeue();
-                if(kvp.Value==nd2)
-                {
-                    connection = kvp.Key;
-                    break;
-                }
-                if(visited.Contains(kvp.Value)){
-                    continue;
-                }
-                visited.Add(kvp.Value);                
-                foreach(var adj in kvp.Value.Adjacents){
-                    queue.Enqueue(new KeyValuePair<string, Node>(kvp.Key+" => "+adj.Name,adj));
-                }
-            }
-            return connection;
+
+            nd1.AddAdjacent(nd2);
+            nd2.AddAdjacent(nd1);
         }
 
-         private class Node{
-            internal IEnumerable<Node> Adjacents{
-                get{
-                    return _adj;
+        public string GetShortestConnection(string name1,string name2){
+            if(string.IsNullOrWhiteSpace(name1) || string.IsNullOrWhiteSpace(name2))
+                return null;
+            
+            var srcNode=GetNode(name1);
+            var destNode=GetNode(name2);
+
+            if(srcNode==null || destNode==null){
+                return null;
+            }
+            string ret="";
+            HashSet<Node> visited=new HashSet<Node>();
+            Queue<KeyValuePair<string,Node>> queueToVisit=new Queue<KeyValuePair<string, Node>>();
+            queueToVisit.Enqueue(new KeyValuePair<string, Node>(srcNode.Name,srcNode));
+
+            while(queueToVisit.Count>0){
+                var item=queueToVisit.Dequeue();
+                if(item.Value==destNode){
+                    ret=item.Key;
+                    break;
+                }
+                if(visited.Contains(item.Value)){
+                    continue;
+                }
+                foreach(var adj in item.Value.Adjacents){
+                    queueToVisit.Enqueue(new KeyValuePair<string, Node>(item.Key+" => "+adj.Name,adj));
                 }
             }
-            internal string Name{
-                get{
-                    return _name;
-                }
-            }
-            private List<Node> _adj=new List<Node>();
+            return ret;
+        }
+
+        private class Node{
+            public string Name=> _name;
+            public IEnumerable<Node> Adjacents=>_adjacents;
             private string _name;
+            private List<Node> _adjacents=new List<Node>();
             internal Node(string name){
                 _name=name;
             }
-            internal void AddConnection(Node nd){
-                if(nd!=null){
-                    if(!_adj.Contains(nd)){
-                        _adj.Add(nd);
-                    }
+
+            internal void AddAdjacent(Node nd){
+                if(!_adjacents.Contains(nd)){
+                    _adjacents.Add(nd);
                 }
             }
         }
     }
+  
 }
